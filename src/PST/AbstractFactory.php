152cc47740d393f181e573d1cdde88958394b21d
Jonathan Brown <jonathan@resultsathand.com>
2018-04-19 16:10:10 -0400

    pst/core#101

--- src/PST/AbstractFactory.php
+++ src/PST/AbstractFactory.php
@@ -462,7 +462,7 @@ abstract class AbstractFactory
 
         curl_setopt( $ch, CURLOPT_URL, $url );
         curl_setopt( $ch, CURLOPT_POST, true );
-        $headers["Content-Type"] = "text/html";
+        $headers["Content-Type"] = "text/xml";
         curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
         curl_setopt( $ch, CURLOPT_POSTFIELDS, $xml_string );
