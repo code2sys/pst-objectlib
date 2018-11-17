0ec9f38a071e3fe4f7a94a47b3b80ad275d3025d
Jonathan Brown <jonathan@resultsathand.com>
2018-11-17 11:47:37 -0500

    Here

--- src/PST/ShowcasePhotoFactory.php
+++ src/PST/ShowcasePhotoFactory.php
@@ -18,7 +18,7 @@ class ShowcasePhotoFactory extends AbstractFactory
     {
         parent::__construct($dbh, $master_factory, $obj, $table, $id);
         $this->_datacols = array(
-            "showcasetrim_id", "crs_photomap_id", "deleted", "url", "short_title"
+            "showcasetrim_id", "crs_photomap_id", "deleted", "url"
         );
     }
 }
