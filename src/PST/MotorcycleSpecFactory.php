7be6b72098f02417dc15e44096dcdcf987fe9673
Jonathan Brown <jonathan@resultsathand.com>
2018-11-17 20:54:43 -0500

    Hide Not Available for pst/Jons-projects#48

--- src/PST/MotorcycleSpecFactory.php
+++ src/PST/MotorcycleSpecFactory.php
@@ -27,4 +27,15 @@ class MotorcycleSpecFactory extends AbstractFactory
     public function getForMotorcycle($motorcycle_id, $data_arrays = false) {
         return $this->fetch(array("motorcycle_id" => $motorcycle_id), $data_arrays);
     }
+
+    public function suppressSpecificNotAvailable($motorcycle_id) {
+        $stmt = $this->dbh->prepare("update motorcyclespec set hidden = 1 where source = 'PST' and final_value = 'Not Available' and motorcycle_id = ?");
+        $stmt->bindValue(1, $motorcycle_id);
+        $stmt->execute();
+    }
+
+    public function suppressAllNotAvailable() {
+        $stmt = $this->dbh->prepare("update motorcyclespec set hidden = 1 where source = 'PST' and final_value = 'Not Available';");
+        $stmt->execute();
+    }
 }
